98debc7dce48b01bbe7ea1277866cd9419edc535
Tristan A. <tristanagraa@gmail.com>
2024-07-27 23:50:47 +0700

    feat: add login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,11 @@
 import { LoginModule } from "@/modules";
 import React from "react";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login | KaryaKita",
+  description: "Login to KaryaKita platform",
+};
 
 const page = () => {
   return <LoginModule />;

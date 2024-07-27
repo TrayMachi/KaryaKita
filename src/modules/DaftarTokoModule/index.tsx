37670941da2f3a1457dd9a54f70ff6356159af77
gracekarinn <gracekarina12@gmail.com>
2024-07-28 03:37:12 +0700

    feat: add daftar toko

---
+++ src/modules/DaftarTokoModule/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BuatTokoForm } from './elements/BuatTokoForm'
+
+const DaftarTokoModule = () => {
+  return (
+    <div>
+      <BuatTokoForm />
+    </div>
+  )
+}
+
+export default DaftarTokoModule

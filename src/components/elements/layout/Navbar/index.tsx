298c01d8b9bb3a812b39c2629e541057357fc4b5
gracekarinn <gracekarina12@gmail.com>
2024-07-27 14:48:39 +0700

    feat: add transition

--- src/components/elements/layout/Navbar/index.tsx
+++ src/components/elements/layout/Navbar/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { TransitionLink } from '@/components/utils/TransitionLink'
 
 export const Navbar = () => {
   return (
-    <div></div>
+    <nav className="flex justify-between items-center p-4">
+      <TransitionLink href="/">Home</TransitionLink>
+      <TransitionLink href="/about">About</TransitionLink>
+    </ nav>
   )
 }

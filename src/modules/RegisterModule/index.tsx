0aa32a3211d45fa83d8814c386afcb801869bdf8
gracekarinn <gracekarina12@gmail.com>
2024-07-27 20:55:19 +0700

    feat: regis ui

---
+++ src/modules/RegisterModule/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Register from './sections/Register'
+
+const RegisterModule = () => {
+  return (
+    <div className='bg-gradient-to-r to-primary from-[#251a56]'>
+      <Register />
+    </div>
+  )
+}
+
+export default RegisterModule
--- src/modules/RegisterModule/sections/Register.tsx
+++ src/modules/RegisterModule/sections/Register.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { RegisterForm } from '../elements/RegisForm'
+import Image from 'next/image'
 
-const RegisterModule = () => {
+const Register = () => {
   return (
     <div>
-      
+      <RegisterForm />
     </div>
   )
 }
 
-export default RegisterModule
+export default Register
